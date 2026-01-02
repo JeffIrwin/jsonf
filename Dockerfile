1ba97cafaa959ddfa2780018896ede3e3e6779cb
Jeff Irwin <jirwin505@gmail.com>
2026-01-02 11:34:45 -0500

    add another test and ci/cd with docker and github actions

---
+++ Dockerfile
@@ -0,0 +1,37 @@
+
+FROM rockylinux:9
+
+RUN dnf update -y && dnf install -y \
+	cmake \
+	gfortran \
+	which
+
+#-------------------------------
+# Install fpm from binary
+WORKDIR /workdir/
+#RUN curl -LO https://github.com/fortran-lang/fpm/releases/download/current/fpm-linux-x86_64
+RUN curl -LO https://github.com/fortran-lang/fpm/releases/download/current/fpm-linux-x86_64-gcc-12
+RUN mv fpm-linux-x86_64* fpm
+RUN chmod +x fpm
+RUN mv fpm /usr/local/bin
+RUN fpm --version
+#-------------------------------
+
+WORKDIR /workdir/jsonf
+
+# The .dockerignore stops ADD from copying build output, e.g. .o files
+ADD . .
+
+#RUN cmake -S . -B build && cmake --build build #--verbose
+RUN fpm test
+RUN fpm test --profile debug
+RUN fpm test --profile release
+
+RUN fpm install --prefix . --profile debug
+RUN ./bin/jsonf -s '{"a123": 69, "x456": 420}'
+RUN ./bin/jsonf -s '{"a123": 69, "nestedaoeuaoeuhtnsaoeuhtnsaoehutns": {"p": 1337, "q": 9999}, "x456": 420}'
+
+RUN fpm install --prefix . --profile release
+RUN ./bin/jsonf -s '{"a123": 69, "x456": 420}'
+RUN ./bin/jsonf -s '{"a123": 69, "nestedaoeuaoeuhtnsaoeuhtnsaoehutns": {"p": 1337, "q": 9999}, "x456": 420}'
+
--- test/test.F90
+++ test/test.F90
@@ -7,35 +7,66 @@ module jsonf__test
 
 contains
 
-subroutine test_in1()
-	character(len=:), allocatable :: str, filename
+#define TEST(val, msg, nfail, ntot) call test_(val, msg, nfail, ntot, __FILE__, __LINE__)
+subroutine test_(val, msg, nfail, ntot, file, line)
+	logical, intent(in) :: val
+	character(len=*), intent(in) :: msg
+	integer, intent(inout) :: nfail, ntot
+	character(len=*), intent(in) :: file
+	integer, intent(in) :: line
+
+	ntot = ntot + 1
+	if (.not. val) then
+		write(*,*) fg_bold_bright_red // "Test failed: " // color_reset
+		write(*,*) "    ", trim(msg)
+		write(*,*) "    at ", trim(file), ":", to_str(line)
+		nfail = nfail + 1
+	end if
+end subroutine test_
+
+
+subroutine test_in1(nfail, ntot)
+	integer, intent(inout) :: nfail, ntot
+	!********
+	character(len=:), allocatable :: str, filename, str_out, expect
 	type(json_t) :: json
 
-	write(*,*) "Starting test_in1()"
-
 	filename = "data/in1.json"
+	write(*,*) "Unit testing file "//quote(filename)//" ..."
+
 	str = read_file(filename)
-	print *, "str = "//LINE_FEED//str
+	!print *, "str = "//LINE_FEED//str
 	call json%read_str(str)
+	str_out = json%to_str()
+	!print *, "str_out = ", str_out
+	expect = &
+		'{' // LINE_FEED // &
+		'    "c": "my str",' // LINE_FEED // &
+		'    "a": 1,' // LINE_FEED // &
+		'    "b": 2,' // LINE_FEED // &
+		'}'
+	TEST(is_str_eq(str_out, expect), "test_basic_jsons 1", nfail, ntot)
 
 end subroutine test_in1
 
-subroutine test_basic_jsons()
+subroutine test_basic_jsons(nfail, ntot)
+	integer, intent(inout) :: nfail, ntot
+	!********
 	character(len=:), allocatable :: str, str_out, expect
 	type(json_t) :: json
+
+	write(*,*) "Unit testing basic JSON strings ..."
 
 	str = '{"a": 1}'
 	call json%read_str(str)
 	str_out = json%to_str()
-	print *, "str_out = ", str_out
+	!print *, "str_out = ", str_out
 	expect = &
 		'{'//LINE_FEED// &
 		'    "a": 1,'//LINE_FEED// &
 		'}'
-	print *, "expect = ", expect
-	if (.not. is_str_eq(str_out, expect)) then
-		call panic("test_basic_jsons test 1 failed")
-	end if
+	!print *, "expect = ", expect
+	TEST(is_str_eq(str_out, expect), "test_basic_jsons 1", nfail, ntot)
 
 end subroutine test_basic_jsons
 
@@ -45,13 +76,22 @@ program test
 
 	use jsonf__test
 	implicit none
+	integer :: nfail, ntot
 	
-	write(*,*) "Starting jsonf test"
+	write(*,*) fg_bright_magenta//"Starting jsonf unit tests"//color_reset
+	nfail = 0
+	ntot = 0
 
-	call test_basic_jsons()
-	call test_in1()
+	call test_basic_jsons(nfail, ntot)
+	call test_in1(nfail, ntot)
 
-	write(*,*) "Finished jsonf test"
+	if (nfail == 0) then
+		write(*, "(a,i0,a)") fg_bold // fg_green // " All ", ntot, " tests passed " // color_reset
+	else
+		write(*, "(a,i0,a,i0,a)") fg_bold_bright_red // " Error: ", nfail, "/", ntot, " tests failed " // color_reset
+	end if
+	if (nfail /= 0) call panic("")
+	call jsonf_exit(EXIT_SUCCESS)
 
 end program test
 
