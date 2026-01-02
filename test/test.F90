7781da773cee609201bcb7249e84fb197dcf01cc
Jeff Irwin <jirwin505@gmail.com>
2026-01-02 11:38:34 -0500

    Revert "break a test"
    This reverts commit a6c3424a2d9c31bff7be666315dd07c5bb23738d.

--- test/test.F90
+++ test/test.F90
@@ -57,7 +57,7 @@ subroutine test_basic_jsons(nfail, ntot)
 
 	write(*,*) "Unit testing basic JSON strings ..."
 
-	str = '{"a": 2}'
+	str = '{"a": 1}'
 	call json%read_str(str)
 	str_out = json%to_str()
 	!print *, "str_out = ", str_out
